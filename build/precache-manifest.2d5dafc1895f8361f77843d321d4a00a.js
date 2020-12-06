0c2e1711b311da77a03aa6e432cd51f59aaae361
Kailey Hart <54502452+KaileyHart@users.noreply.github.com>
2020-12-06 12:15:41 -0500

    Added Build files

---
+++ build/precache-manifest.2d5dafc1895f8361f77843d321d4a00a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "045299a6518618dc5f72",
+    "url": "/static/css/main.714b14d5.chunk.css"
+  },
+  {
+    "revision": "045299a6518618dc5f72",
+    "url": "/static/js/main.045299a6.chunk.js"
+  },
+  {
+    "revision": "7dfbc94f869b9e815529",
+    "url": "/static/js/1.7dfbc94f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44a5fcc2688f88cce5ea4d93791ccd17",
+    "url": "/static/media/MainHeader.44a5fcc2.png"
+  },
+  {
+    "revision": "3b2d2d64d1f960a9a75d65f99817d65d",
+    "url": "/static/media/Footer.3b2d2d64.png"
+  },
+  {
+    "revision": "66b478beaf8ccd30087792c6b8ac78a3",
+    "url": "/static/media/return-arrow-curve-pointing-left.66b478be.svg"
+  },
+  {
+    "revision": "7d9ea7da9ac2f20911a50c4464567492",
+    "url": "/static/media/list.7d9ea7da.svg"
+  },
+  {
+    "revision": "8f4534b6fa8ae104411cd17c6c06b658",
+    "url": "/static/media/plus.8f4534b6.svg"
+  },
+  {
+    "revision": "454869d028e964f34f074a3c73e09ba3",
+    "url": "/static/media/controls.454869d0.svg"
+  },
+  {
+    "revision": "548d396465eab402d9fe52031697ad3e",
+    "url": "/index.html"
+  }
+];
